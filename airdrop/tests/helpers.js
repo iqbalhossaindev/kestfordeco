4612b674d42bd4ea41e1d4233f25ca43cb9e0140
Iqbal Hossain <271834538+iqbalhossaindev@users.noreply.github.com>
2026-04-20 21:01:44 +0400

    Add files via upload

--- airdrop/tests/helpers.js
+++ airdrop/tests/helpers.js
@@ -1,1 +1,42 @@
+const { EventEmitter } = require('events');
 
+function invoke(handler, { method = 'GET', url = '/', headers = {}, body } = {}) {
+  return new Promise((resolve, reject) => {
+    const req = new EventEmitter();
+    req.method = method;
+    req.url = url;
+    req.headers = headers;
+    req.body = body;
+
+    const res = {
+      statusCode: 200,
+      headers: {},
+      setHeader(key, value) {
+        this.headers[key.toLowerCase()] = value;
+      },
+      end(payload) {
+        this.payload = payload;
+        try {
+          this.json = payload ? JSON.parse(payload) : null;
+        } catch (error) {
+          this.json = null;
+        }
+        resolve(this);
+      }
+    };
+
+    try {
+      const maybePromise = handler(req, res);
+      Promise.resolve(maybePromise).catch(reject);
+      process.nextTick(() => {
+        if (body === undefined) {
+          req.emit('end');
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+module.exports = { invoke };
